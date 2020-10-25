71039d9af1f70d6564dd7b4a31fb16e2f5bd9451
baek <xemeal@gmail.com>
2020-10-25 17:09:28 +0900

    typescript 적용

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,25 +6,27 @@ interface State {
   username: string
 }
 
+
+
 class App extends React.Component<Props,State> {
 
-  // constructor(props:Props) {
-  //   super(props);
-  //   this.state = {
-  //       username:null
-  //   };
-  // }
+  constructor(props:Props) {
+    super(props);
+    this.state = {
+        username:null
+    };
+  }
  
   state: State  = {
     username: ""
   }
-
+  
   componentDidMount() {
     fetch('http://localhost:3001/api')
     .then(res => res.json())
     .then(data => 
       this.setState({username:data.username}));
-  }
+  };
 
   render() {
     return (
