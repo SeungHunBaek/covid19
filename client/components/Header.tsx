764be709042f415bec39b4c3d3ec9f419e80a6d3
baek <xemeal@gmail.com>
2020-11-12 10:57:31 +0900

    header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-
 const Wrapper = styled.div`
     /* 레이아웃 */
     display: flex;
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader'
+      },
+      { 
+        test: /\.css$/, 
+        use: 'css-loader' 
       }
     ]
   },
