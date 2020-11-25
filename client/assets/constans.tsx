7c2d792a03c714fbccf312c2ebae8ea9ee57cd51
baek <xemeal@gmail.com>
2020-11-25 17:49:39 +0900

    constans

---
+++ client/assets/constans.tsx
@@ -0,0 +1,46 @@
+//아메리카
+export const AMERICA_SEQ        = 10866;
+export const CANADA_SEQ         = 10867;
+export const BRAZIL_SEQ         = 10868;
+export const MAXICO_SEQ         = 10869;
+// 아시아
+export const KOREA_SEQ          = 10818;
+export const CHINA_SEQ          = 10819;
+export const HONKONG_SEQ        = 10820;
+export const TAIWAN_SEQ         = 10821;
+export const JAPAN_SEQ          = 10823;
+export const SINGAPORE_SEQ      = 10824;
+export const INDIA_SEQ          = 10828;
+export const RUSSIA_SEQ         = 10832;
+//유럽
+export const ITALY_SEQ          = 10901;
+export const GERMANY_SEQ        = 10902;
+export const FRANCE_SEQ         = 10903;
+export const UK_SEQ             = 10904;
+export const SPAIN_SEQ          = 10905;
+
+//오세아니아
+export const AUSTRALIA_SEQ      = 10949;
+export const NEWZEALAND_SEQ     = 10950;
+
+//아메리카
+export const AMERICA_NAME        = "United States of America";
+export const CANADA_NAME         = "Canada";
+export const BRAZIL_NAME         = "Brazil";
+export const MAXICO_NAME         = "Mexico";
+// 아시아
+export const KOREA_NAME          = "South Korea";
+export const CHINA_NAME          = "China";
+export const HONKONG_NAME        = "Hong Kong";
+export const JAPAN_NAME          = "Japan";
+export const RUSSIA_NAME         = "Russia";
+//유럽
+export const ITALY_NAME          = "Italy";
+export const GERMANY_NAME        = "Germany";
+export const FRANCE_NAME         = "France";
+export const UK_NAME             = "英国";
+export const SPAIN_NAME          = "Spain";
+
+//오세아니아
+export const AUSTRALIA_NAME      = "Australia";
+export const NEWZEALAND_NAME     = "New Zealand";
