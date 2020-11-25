97ec9d1d25915c4007263bc9ecbc7c681327b2d2
baek <xemeal@gmail.com>
2020-11-25 17:19:56 +0900

    Navbar수정

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -8,18 +8,13 @@ const Navigation = (props) => {
     <Navbar.Brand href="/">Covid19 Status</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
+      
       <Nav className="mr-auto">
-        <Nav.Link ><Link to="/">Introduce</Link></Nav.Link>
-        <Nav.Link ><Link to="/koreaData">Korea Data</Link></Nav.Link>
-        <Nav.Link ><Link to="/globalData">Global Data</Link></Nav.Link>
-        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown> */}
+        <Nav.Link as={Link} to="/" >Introduce</Nav.Link>
+        <Nav.Link as={Link} to="/koreaData">Korea Data</Nav.Link>
+        <Nav.Link as={Link} to="/globalData">Global Data</Nav.Link>
       </Nav>
+      
       <Nav>
         <Nav.Link href="#deets">More deets</Nav.Link>
         <Nav.Link eventKey={2} href="#memes">
