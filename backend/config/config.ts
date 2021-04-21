1c61bc916709a3bdf8a59ac932004fb2af76cb89
baek <xemeal@gmail.com>
2021-04-21 22:42:13 +0900

    공공데이터 api연동

---
+++ backend/config/config.ts
@@ -0,0 +1,10 @@
+export default {
+    InfState: {
+        "endPoint": "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?",
+        "key": "serviceKey=e%2Fu5HiiZcpglw2q8nndKG60KcqEAnOaOLYiLGQJZa4klvndij7SlvvQAxvRLifAESwNq5IKZH4lVjeSd5uS%2FWQ%3D%3D",
+        "pageNo": "&pageNo=1",
+        "numOfRows": "&numOfRows=10000",
+        "startCreateDt": "&startCreateDt=2020031",
+        "endCreateDt": "&endCreateDt=20210315"    
+    }
+} 
