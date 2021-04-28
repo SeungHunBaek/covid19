dca12789c1278ca000dcc7a625b3065720823bc1
baek <xemeal@gmail.com>
2021-04-28 20:05:43 +0900

    layout main

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                main
+            </div>
+        )
+    }
+
+}
+
+
+export default Main;
