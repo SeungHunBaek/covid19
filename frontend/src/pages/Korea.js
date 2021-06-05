e93eaf253e5c180341686ec2abbf4f84ae90c2e3
baek <xemeal@gmail.com>
2021-06-05 18:43:54 +0900

    국내 지역데이터 취득

--- frontend/src/pages/Korea.js
+++ frontend/src/pages/Korea.js
@@ -12,13 +12,19 @@ class Korea extends React.Component {
     }
     //React 엘리먼트를 실제 DOM 노드에 추가하기 직전에 호출.
     async componentWillMount() {
+        this.getDomesticData()
+
+    }
+    // 국내 데이터
+    getDomesticData() {
         const url = `http://localhost:3000/korea-data`;
  
-        const {data:{items:{item}} } = await axios.get(url)
+        const {data:{items:{item}}} = await axios.get(url)
         let chartData = []
 
         console.log(item);
-        for (let i = item.length-1; 0 <= i; i--) {
+        for (let i = item.length-1, j = 0; 7 > j; i--) {
+            j++;
             let date = item[i].stateDt + "";
             console.log(date.substring(0,4)+date.substring(4,6)+date.substring(6,8))
             chartData.push({
@@ -31,7 +37,10 @@ class Korea extends React.Component {
         // this.setState({ worldData: item });
         this.setState({datas :chartData});
     }
-   
+    // 국내 지역별 데이터 합계
+    getDomesticRegionData() {
+        
+    }
 
     render() {
         return (
