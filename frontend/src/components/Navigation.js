fb879750e33ecd7d4757248f53322376d64d1913
baek <xemeal@gmail.com>
2021-07-01 19:49:02 +0900

    네비게이터 디자인

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,14 +5,27 @@ function Navigation() {
   return (
       <div className="Navigation-align">
           <div className="Navigation-content">
-            <ul>
-              <li>
-               <Link to='/'>home</Link>
-              </li>
-              <li>
-               <Link to='/korea'>korea</Link>
-              </li>
-            </ul>
+               <Link to='/' style={{ textDecoration: 'none' }}>
+                    <span className='button'>
+                         개 요
+                    </span>
+                 </Link>
+
+               <Link to='/korea' style={{ textDecoration: 'none' }}>
+                    <span className='button'>
+                         국내현황
+                    </span>
+               </Link>
+               <Link to='/world' style={{ textDecoration: 'none' }}>
+                    <span className='button'>
+                         세계현황
+                    </span>
+               </Link>
+               <Link to='/vaccine' style={{ textDecoration: 'none' }}>
+                    <span className='button'>
+                         접종현황
+                    </span>
+               </Link>
           </div>
       </div>
   );
