f7b0fdc8c2956485e021959e9e1b510ff0b10c0a
baek <xemeal@gmail.com>
2021-08-16 18:21:35 +0900

    layout

--- frontend/src/components/SubTab.js
+++ frontend/src/components/SubTab.js
@@ -8,24 +8,24 @@ class SubTab extends React.Component {
     }
     render() {
         let selectedCharts;
-        if(this.props.chartState === 0){            
+        if(this.props.chartState === 1){            
             selectedCharts =
                 <div className='align-row'>
-                    <span className='selected-charts' onClick={this.props.cumulative}>
-                        ● 누진 확진자 추이
-                    </span>
                     <span className='subTab' onClick={this.props.daily}>
                         ◯ 일별 신규 확진자수
+                    </span>
+                    <span className='selected-charts' onClick={this.props.cumulative}>
+                        ● 누진 확진자 추이
                     </span>
                 </div>
         } else {
             selectedCharts =
                 <div className='align-row'>
-                    <span className='subTab' onClick={this.props.cumulative}>
-                        ◯ 누진 확진자 추이
-                    </span>
                     <span className='selected-charts' onClick={this.props.daily}>
                         ● 일별 신규 확진자수
+                    </span>
+                    <span className='subTab' onClick={this.props.cumulative}>
+                        ◯ 누진 확진자 추이
                     </span>
                 </div>
         }
